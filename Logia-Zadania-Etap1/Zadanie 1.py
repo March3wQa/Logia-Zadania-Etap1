fe8410ec75075d47ff3f15f999f4cb0f5de9851c
Piotr Madura <piotrek.marchewka123@gmail.com>
2018-10-14 18:43:12 +0200

    Zad1: Okna

--- Logia-Zadania-Etap1/Zadanie 1.py
+++ Logia-Zadania-Etap1/Zadanie 1.py
@@ -21,7 +21,49 @@ def domki(n):
 	t.pu()
 	##############
 
+	### OKNA ###
+	t.fillcolor('cyan')
 
+	t.goto(pos[0]+20, pos[1]+20)
+	t.pd()
+	t.begin_fill()
+	t.goto(pos[0]+40, pos[1]+20)
+	t.goto(pos[0]+40, pos[1]+40)
+	t.goto(pos[0]+20, pos[1]+40)
+	t.goto(pos[0]+20, pos[1]+20)
+	t.end_fill()
+	t.pu()
+
+	t.goto(pos[0]+60, pos[1]+20)
+	t.pd()
+	t.begin_fill()
+	t.goto(pos[0]+80, pos[1]+20)
+	t.goto(pos[0]+80, pos[1]+40)
+	t.goto(pos[0]+60, pos[1]+40)
+	t.goto(pos[0]+60, pos[1]+20)
+	t.end_fill()
+	t.pu()
+
+	t.goto(pos[0]+60, pos[1]+60)
+	t.pd()
+	t.begin_fill()
+	t.goto(pos[0]+80, pos[1]+60)
+	t.goto(pos[0]+80, pos[1]+80)
+	t.goto(pos[0]+60, pos[1]+80)
+	t.goto(pos[0]+60, pos[1]+60)
+	t.end_fill()
+	t.pu()
+
+	t.goto(pos[0]+20, pos[1]+60)
+	t.pd()
+	t.begin_fill()
+	t.goto(pos[0]+40, pos[1]+60)
+	t.goto(pos[0]+40, pos[1]+80)
+	t.goto(pos[0]+20, pos[1]+80)
+	t.goto(pos[0]+20, pos[1]+60)
+	t.end_fill()
+	t.pu()
+	############
 
 
 
