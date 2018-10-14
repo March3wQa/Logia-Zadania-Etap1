88b3e22a8f06d43e789dfed6101d1cfeb27335df
Piotr Madura <piotrek.marchewka123@gmail.com>
2018-10-14 20:24:27 +0200

    Zad2: *Koniec

---
+++ Logia-Zadania-Etap1/Zadanie 2.py
@@ -0,0 +1,32 @@
+def o6(max):
+    try:
+        if max<12 or max>10000:
+            raise ValueError('Błędna liczba. Akceptowane liczby to od 12 do 10000.')
+    except ValueError as err:
+        return err
+    else:
+        primes = [2, 3]
+        n = 4
+        for _ in range(max):
+            tries = 0
+            for i in range(len(primes)):
+                if n%primes[i] != 0:
+                    tries += 1
+            if tries == len(primes):
+                primes.append(n)
+            n += 1
+        while primes[len(primes)-1] > max:
+            del primes[len(primes)-1]
+        else:
+            try:
+                for k in primes:
+                    c1 = primes[len(primes)-k]
+                    c2 = primes[len(primes)-(k+1)]
+                    if c1-6 == c2:
+                        return c1
+            except IndexError:
+                for k in primes:
+                    c1 = primes[len(primes)-1]
+                    c2 = primes[len(primes)-(k+1)]
+                    if c1-6 == c2:
+                        return c1
